e8f9f2d6779a95a27a91c5acf83ee62b20e684cc
Mike Laurel <mlaurel@hey.com>
2020-12-10 17:15:13 -0800

    Create first Thread entity type and fields

---
+++ goreddit.go
@@ -0,0 +1,9 @@
+package goreddit
+
+import "github.com/google/uuid"
+
+type Thread struct {
+	ID          uuid.UUID `db:"id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+}
